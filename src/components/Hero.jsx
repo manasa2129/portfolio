5222ff9ff54f5222952cc4d8875cc9a300da9636
manasa2129 <111952008+manasa2129@users.noreply.github.com>
2025-07-09 04:31:39 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         </p>
         <div className="hero-buttons">
           <a href="#projects" className="btn">View Projects</a>
-          <a href="/Manasa_Resume_.pdf" className="btn" download>Download Resume</a>
+          <a href="/portfolio/Manasa_Resume_.pdf" className="btn" download>Download Resume</a>
         </div>
       </div>
     </section>
